a948a3cab3638907d269446f701d0fb0c1853ca5
n.khadasevich <nataliabizunkova@gmail.com>
2024-02-18 21:33:52 +0300

    delete HelloWorld

--- HelloWorld.java
+++
@@ -1,9 +0,0 @@
-public class HelloWorld{
-public static void main(String[] args){
-User user=new User();
-user.name="Natalia";
-user.password="qwer3456sfxjx";
-User user2=new User();
-user2.name="Masha";
-user2.password="wsdfgbn567";
-}}
