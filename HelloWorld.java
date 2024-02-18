a5515047df31844ad3d8dbead28a2a8f3990c2e3
n.khadasevich <nataliabizunkova@gmail.com>
2024-02-18 21:23:14 +0300

    add new pwd

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,7 +2,7 @@ public class HelloWorld{
 public static void main(String[] args){
 User user=new User();
 user.name="Natalia";
-user.password="qwer3456";
+user.password="qwer3456sfxjx";
 User user2=new User();
 user2.name="Masha";
 user2.password="wsdfgbn567";
